5bca8d34d39beff9d2a027791009c707c8eb60aa
accaicedtea <abdelaziz.1941255@studenti.uniroma1.it>
2025-11-20 17:01:20 +0100

    Update database config with environment support

--- config/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array (
-  'api_base_url' => 'https://accaicedtea.altervista.org',
-  'project_name' => 'My API',
-  'api_version' => '1.0',
-  'environment' => 'development',
-  'contact_email' => 'admin@tuosito.com',
-);
--- config/database.php
+++ config/database.php
@@ -1,13 +1,26 @@
 <?php
-// MySQL
-return [
-    'dbType' => 'MySQL',
-    'host' => 'localhost',
-    'dbname' => 'my_menu',
-    'user' => 'root',
-    'pass' => '',
-    'charset' => 'utf8mb4',
-];
-// Pg SQL
 
+// Configuration for different environments
+$environment = $_ENV['APP_ENV'] ?? 'development';
+
+$databases = [
+    'development' => [
+        'dbType' => 'MySQL',
+        'host' => 'localhost',
+        'dbname' => 'my_menu',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8mb4',
+    ],
+    'production' => [
+        'dbType' => 'MySQL',
+        'host' => 'localhost',
+        'dbname' => 'my_menu_prod',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8mb4',
+    ],
+];
+
+return $databases[$environment];
 
